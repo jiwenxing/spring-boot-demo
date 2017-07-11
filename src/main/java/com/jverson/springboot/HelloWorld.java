d5477d0f10c20e88426e15baf02ff97c36d6f4cb
360BUYAD\jiwenxing <jiwenxing@jd.com>
2017-07-11 21:09:55 +0800

    backup

--- src/main/java/com/jverson/springboot/HelloWorld.java
+++ src/main/java/com/jverson/springboot/HelloWorld.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+//@SpringBootApplication 
 public class HelloWorld {
 
 	@RequestMapping("/")
