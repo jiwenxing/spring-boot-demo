9f0b78405131e94ef0d4dbe94828b503c4640440
360BUYAD\jiwenxing <jiwenxing@jd.com>
2017-07-11 16:58:50 +0800

    helloworld

---
+++ src/main/java/com/jverson/springboot/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.jverson.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWorld {
+
+	@RequestMapping("/")
+	String home(){
+		return "hello world!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorld.class, args);
+	}
+	
+}
