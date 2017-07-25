be8359b3819efab9fe037553b2f31ba634f7e75f
jiwenxing <blite1228@163.com>
2017-07-25 22:37:59 +0800

    config

--- src/main/java/com/jverson/springboot/conf/BeanConf.java
+++ src/main/java/com/jverson/springboot/conf/BeanConf.java
@@ -1,9 +1,12 @@
 package com.jverson.springboot.conf;
 
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
 
 import com.jverson.springboot.bean.Person;
 import com.jverson.springboot.bean.Person2;
@@ -22,6 +25,20 @@ public class BeanConf {
 	@ConfigurationProperties(prefix = "person2")
 	public Person2 person2() {
 		return new Person2();
+	}
+	
+	/**
+	 * Roughly equivalent to the error-page element traditionally found in web.xml
+	 */
+//	@Bean   //uncomment this to enable
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+	   return (container -> {
+	        ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+	        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+	        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+
+	        container.addErrorPages(error401Page, error404Page, error500Page);
+	   });
 	}
 	
 }
--- src/main/java/com/jverson/springboot/controller/mvc/HelloController.java
+++ src/main/java/com/jverson/springboot/controller/mvc/HelloController.java
@@ -5,6 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.jverson.springboot.common.ErrorEnum;
+import com.jverson.springboot.common.PageException;
+import com.jverson.springboot.common.RestException;
+
 @Controller
 public class HelloController {
 
@@ -12,6 +16,16 @@ public class HelloController {
 	public String Hello(Model model){
 		model.addAttribute("name", "jverson");
 		return "welcome";
+	}
+	
+	@RequestMapping("/json")
+	public String jsonException() throws RestException{
+		throw new RestException(ErrorEnum.ERROR_PARAMETERS);
+	}
+	
+	@RequestMapping("/page")
+	public String pageException() throws PageException{
+		throw new PageException(ErrorEnum.ERROR_SERVER);
 	}
 	
 }
