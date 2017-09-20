41cfa0084a4360fb2cecd4593446ad25e693e12c
jiwenxing <blite1228@163.com>
2017-09-20 21:39:13 +0800

    Tomca-jdbc pool

---
+++ src/test/java/com/jverson/test/database/DataSourceTest.java
@@ -0,0 +1,59 @@
+package com.jverson.test.database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+
+public class DataSourceTest {
+
+	public static void main(String[] args) {
+		PoolProperties p = new PoolProperties();
+        p.setUrl("jdbc:MySql://192.168.192.125:3358/spring_boot_test?useUnicode=true");
+        p.setDriverClassName("com.mysql.jdbc.Driver");
+        p.setUsername("root");
+        p.setPassword("123456");
+        p.setJmxEnabled(true);
+        p.setTestWhileIdle(false);
+        p.setTestOnBorrow(true);
+        p.setValidationQuery("SELECT 1");
+        p.setTestOnReturn(false);
+        p.setValidationInterval(30000);
+        p.setTimeBetweenEvictionRunsMillis(30000);
+        p.setMaxActive(100);
+        p.setInitialSize(10);
+        p.setMaxWait(10000);
+        p.setRemoveAbandonedTimeout(60);
+        p.setMinEvictableIdleTimeMillis(30000);
+        p.setMinIdle(10);
+        p.setLogAbandoned(true);
+        p.setRemoveAbandoned(true);
+        p.setJdbcInterceptors(
+          "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"+
+          "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+        DataSource datasource = new DataSource();
+        datasource.setPoolProperties(p);
+
+        Connection con = null;
+        try {
+          con = datasource.getConnection();
+          Statement st = con.createStatement();
+          ResultSet rs = st.executeQuery("select * from user");
+          int cnt = 1;
+          while (rs.next()) {
+              System.out.println((cnt++)+". name:" +rs.getString("name")+
+                " age:"+rs.getString("age"));
+          }
+          rs.close();
+          st.close();
+        } catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+          if (con!=null) try {con.close();}catch (Exception ignore) {}
+        }
+	}
+	
+}
