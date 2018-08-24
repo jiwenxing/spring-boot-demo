95c08131ce8ed1624d17c17e0e7b7309bb3d084d
Jverson <blite1228@163.com>
2018-08-24 10:55:18 +0800

    add gitbook

---
+++ src/main/java/com/jverson/springboot/mq/Receiver.java
@@ -0,0 +1,20 @@
+package com.jverson.springboot.mq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "task")
+public class Receiver {
+
+	private static Logger logger = LoggerFactory.getLogger(Receiver.class.getName());
+	
+	@RabbitHandler
+    public void process(String interfaceName) {
+		logger.info("Receiver : " + interfaceName);
+    }
+	
+}
